0692b9947b21064d41534f619bf3373288804c4e
unknown <sashabidardel@gmail.com>
2021-11-14 20:15:20 +0100

    subiendo contar ovejas con modulos

--- contarOveyas.js
+++ contarOveyas.js
@@ -1,1 +1,18 @@
-export 
+export function contar() {
+    let lista = [
+        true, true,  true,  false,
+        true,  true,  true,  true ,
+        true,  false, true,  false,
+        true,  false, false, true ,
+        true,  true,  true,  true ,
+        false, false, true,  true
+    ]
+    let cont=0;
+    
+        for (let index = 0; index < lista.length; index++) {
+            if (lista[index]==true) {
+                cont++;
+            }
+         }
+    return cont;
+ }
--- numeroOveyas.js
+++ numeroOveyas.js
@@ -1,22 +1,5 @@
 
-function contar() {
-    let lista = [
-        true, true,  true,  false,
-        true,  true,  true,  true ,
-        true,  false, true,  false,
-        true,  false, false, true ,
-        true,  true,  true,  true ,
-        false, false, true,  true
-    ]
-    let cont=0;
-    
-        for (let index = 0; index < lista.length; index++) {
-            if (lista[index]==true) {
-                cont++;
-            }
-         }
-    return cont;
- }
+import { contar } from "./contarOveyas.js";
 
 
 function numeroOvejas() {
